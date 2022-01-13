9fe50d53a8fea3a28c7699ea1fd5fe203ef7d6d5
Evgeny.Shinkarev@urfu.me <Evgeny.Shinkarev@urfu.me>
2022-01-13 16:16:31 +0500

    доделал скритп быстрого создания анимации

--- src/new-animation.ts
+++ src/new-animation.ts
@@ -1,5 +1,14 @@
+function offset(element: HTMLElement){
+    const rect = element.getBoundingClientRect();
+    const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const top = rect.top + scrollTop;
+    const left = rect.left + screenLeft;
+    return {top: top, left: left, bottom: top + element.offsetHeight, right: left + element.offsetWidth};
+}
+
 function parseCSS(strCSS: string){
-    const pattern = new RegExp(".*{", "g");
+    const pattern = new RegExp(String.raw`\.[\w|\d|\-]{0,}`, "g");
     let selectors = [...new Set(Array.from(strCSS.matchAll(pattern), m => m[0]))];
     selectors = selectors.filter(s => {
         if (!s.startsWith(".")){
@@ -7,7 +16,7 @@ function parseCSS(strCSS: string){
             return false;
         }
         return true;
-    }).map(s => s.slice(1, -1));
+    }).map(s => s.slice(1));
 
     return [selectors[0], selectors.slice(1)];
 }
@@ -23,42 +32,76 @@ function selectElementsToAnime(animeExecClasses: string[]){
     return Array.from(allAnimeElements);
 }
 
-// Нужно дописать регулярку и выделить значение свойства
 function makeSelectorPropertyValueDict(selectors: string[], property: string, strCSS: string){
     const d: {[key: string]: string} = {};
     selectors.forEach(selector => {
-        debugger;
-        const pattern = new RegExp(String.raw`${selector}{.{0,4}${property}.{0,1}:`, "gs");
-        const values = Array.from(strCSS.matchAll(pattern), m => m[0]);
-        if (values.length > 1)
-            throw new Error(`css property ${property} was overwritten by ${values.slice(1).join(", ")}. Create new classes vice cascading.`);
-        console.log(`${values} of ${property} for selector ${selector}`);
+        const patternPropertyValue = new RegExp(String.raw`${property}\s{0,}:\s{0,}[\w|\-]{0,}\s{0,};`)
+        const patternSelectorProperty = new RegExp(String.raw`\.${selector}\{[^\}]{0,}${property}\s{0,}:\s{0,}[\w|\-|\d]{0,}\s{0,};`, "gs");
+        const selectorProperties = Array.from(strCSS.matchAll(patternSelectorProperty), m => m[0]);
+
+        if (selectorProperties.length > 1)
+            throw new Error(`css property ${property} was overwritten by ${selectorProperties.slice(1).join(", ")}. Create new classes vice cascading.`);
+        if (selectorProperties.length == 0)
+            console.log(`class ${selector} doesn't contain property ${property} or RegExp pattern has bag`);
+        else{
+            const propertyValueList = selectorProperties[0].match(patternPropertyValue);
+            if (!propertyValueList || propertyValueList.length == 0)
+                throw new Error(`pattern ${patternSelectorProperty} found wanted string, but pattern ${patternPropertyValue} which incule in previous pattern didn't find wanted part of the string`);
+            if (propertyValueList.length > 1)
+                console.log(`found few properties with equal key in one class. Should remove not used.`);
+            const propertyValue = propertyValueList[propertyValueList.length - 1];
+            const value = propertyValue.slice(propertyValue.indexOf(":") + 1, propertyValue.indexOf(";")).trim();
+            d[selector] = value;
+            console.log(`${value} is value of property ${property} for selector ${selector}`);
+        }
     });
+
+    return d;
 }
 
-// дописать замену классов на один и переопределение style animation-name
 function combineAnimeClasses(animeElements: Element[], animeExecClasses: string[], strCss: string){
     const selectorValueDict = makeSelectorPropertyValueDict(animeExecClasses, "animation-name", strCss);
     animeElements.forEach(animeElement => {
-        const intersect: string[] = [];
+        const intersectAnimeClasses: string[] = [];
+        debugger;
         animeElement.classList.forEach(className =>{
-            if (className in animeExecClasses)
-                intersect.push(className);
+            if (animeExecClasses.includes(className))
+                intersectAnimeClasses.push(className);
         });
-    })
+        const animeNames: string[] = [];
+        intersectAnimeClasses.forEach(className => {
+            animeNames.push(selectorValueDict[className]);
+        });
+        (animeElement as HTMLElement).style.animationName = animeNames.join(", ");
+    });
 }
 
-//дописать обработк event
+function setAnimeStartAtScroll(animeElements: HTMLElement[]){
+    const screenHeight = document.documentElement.clientHeight;
+    const topStart = screenHeight / 3 * 2;
+    animeElements.forEach(animeElement => {
+        const animeElementOffset = offset(animeElement);
+        addEventListener("scroll", () => {
+            if (screenHeight + scrollY > animeElementOffset.bottom || topStart + scrollY > animeElementOffset.top)
+                animeElement.style.animationPlayState = "running";
+        });
+    });
+}
+
 async function initAnimation(nameCSS: string){
+    const defaultOnScroll = window.onscroll;
+    window.onscroll = () => window.scrollTo(0, 0);
     const strCSS = await fetch(`../css/${nameCSS}`).then(response => response.text());
+    window.onscroll = defaultOnScroll;
     const selectors = parseCSS(strCSS);
     const animeStartClass = selectors[0] as string;
     const animeExecClasses = selectors[1] as string[];
     const animeElements = selectElementsToAnime(animeExecClasses);
     combineAnimeClasses(animeElements, animeExecClasses, strCSS);
-    console.log(animeStartClass);
-    console.log(animeExecClasses);
-    console.log(animeElements);
+    setAnimeStartAtScroll(animeElements as HTMLElement[]);
+    console.log(animeStartClass + " class used for anime start");
+    console.log(animeExecClasses + " classes used for define anime");
+    console.log(animeElements + " elements from html page, which will anime");
 }
 
 initAnimation("new-animation.css");
--- tscompiled/new-animation.js
+++ tscompiled/new-animation.js
@@ -1,6 +1,14 @@
 "use strict";
+function offset(element) {
+    const rect = element.getBoundingClientRect();
+    const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const top = rect.top + scrollTop;
+    const left = rect.left + screenLeft;
+    return { top: top, left: left, bottom: top + element.offsetHeight, right: left + element.offsetWidth };
+}
 function parseCSS(strCSS) {
-    const pattern = new RegExp(".*{", "g");
+    const pattern = new RegExp(String.raw `\.[\w|\d|\-]{0,}`, "g");
     let selectors = [...new Set(Array.from(strCSS.matchAll(pattern), m => m[0]))];
     selectors = selectors.filter(s => {
         if (!s.startsWith(".")) {
@@ -8,7 +16,7 @@ function parseCSS(strCSS) {
             return false;
         }
         return true;
-    }).map(s => s.slice(1, -1));
+    }).map(s => s.slice(1));
     return [selectors[0], selectors.slice(1)];
 }
 function selectElementsToAnime(animeExecClasses) {
@@ -20,40 +28,71 @@ function selectElementsToAnime(animeExecClasses) {
     });
     return Array.from(allAnimeElements);
 }
-// Нужно дописать регулярку и выделить значение свойства
 function makeSelectorPropertyValueDict(selectors, property, strCSS) {
     const d = {};
     selectors.forEach(selector => {
-        debugger;
-        const pattern = new RegExp(String.raw `${selector}{.{0,4}${property}.{0,1}:`, "gs");
-        const values = Array.from(strCSS.matchAll(pattern), m => m[0]);
-        if (values.length > 1)
-            throw new Error(`css property ${property} was overwritten by ${values.slice(1).join(", ")}. Create new classes vice cascading.`);
-        console.log(`${values} of ${property} for selector ${selector}`);
+        const patternPropertyValue = new RegExp(String.raw `${property}\s{0,}:\s{0,}[\w|\-]{0,}\s{0,};`);
+        const patternSelectorProperty = new RegExp(String.raw `\.${selector}\{[^\}]{0,}${property}\s{0,}:\s{0,}[\w|\-|\d]{0,}\s{0,};`, "gs");
+        const selectorProperties = Array.from(strCSS.matchAll(patternSelectorProperty), m => m[0]);
+        if (selectorProperties.length > 1)
+            throw new Error(`css property ${property} was overwritten by ${selectorProperties.slice(1).join(", ")}. Create new classes vice cascading.`);
+        if (selectorProperties.length == 0)
+            console.log(`class ${selector} doesn't contain property ${property} or RegExp pattern has bag`);
+        else {
+            const propertyValueList = selectorProperties[0].match(patternPropertyValue);
+            if (!propertyValueList || propertyValueList.length == 0)
+                throw new Error(`pattern ${patternSelectorProperty} found wanted string, but pattern ${patternPropertyValue} which incule in previous pattern didn't find wanted part of the string`);
+            if (propertyValueList.length > 1)
+                console.log(`found few properties with equal key in one class. Should remove not used.`);
+            const propertyValue = propertyValueList[propertyValueList.length - 1];
+            const value = propertyValue.slice(propertyValue.indexOf(":") + 1, propertyValue.indexOf(";")).trim();
+            d[selector] = value;
+            console.log(`${value} is value of property ${property} for selector ${selector}`);
+        }
     });
+    return d;
 }
-// дописать замену классов на один и переопределение style animation-name
 function combineAnimeClasses(animeElements, animeExecClasses, strCss) {
     const selectorValueDict = makeSelectorPropertyValueDict(animeExecClasses, "animation-name", strCss);
     animeElements.forEach(animeElement => {
-        const intersect = [];
+        const intersectAnimeClasses = [];
+        debugger;
         animeElement.classList.forEach(className => {
-            if (className in animeExecClasses)
-                intersect.push(className);
+            if (animeExecClasses.includes(className))
+                intersectAnimeClasses.push(className);
+        });
+        const animeNames = [];
+        intersectAnimeClasses.forEach(className => {
+            animeNames.push(selectorValueDict[className]);
+        });
+        animeElement.style.animationName = animeNames.join(", ");
+    });
+}
+function setAnimeStartAtScroll(animeElements) {
+    const screenHeight = document.documentElement.clientHeight;
+    const topStart = screenHeight / 3 * 2;
+    animeElements.forEach(animeElement => {
+        const animeElementOffset = offset(animeElement);
+        addEventListener("scroll", () => {
+            if (screenHeight + scrollY > animeElementOffset.bottom || topStart + scrollY > animeElementOffset.top)
+                animeElement.style.animationPlayState = "running";
         });
     });
 }
-//дописать обработк event
 async function initAnimation(nameCSS) {
+    const defaultOnScroll = window.onscroll;
+    window.onscroll = () => window.scrollTo(0, 0);
     const strCSS = await fetch(`../css/${nameCSS}`).then(response => response.text());
+    window.onscroll = defaultOnScroll;
     const selectors = parseCSS(strCSS);
     const animeStartClass = selectors[0];
     const animeExecClasses = selectors[1];
     const animeElements = selectElementsToAnime(animeExecClasses);
     combineAnimeClasses(animeElements, animeExecClasses, strCSS);
-    console.log(animeStartClass);
-    console.log(animeExecClasses);
-    console.log(animeElements);
+    setAnimeStartAtScroll(animeElements);
+    console.log(animeStartClass + " class used for anime start");
+    console.log(animeExecClasses + " classes used for define anime");
+    console.log(animeElements + " elements from html page, which will anime");
 }
 initAnimation("new-animation.css");
 //# sourceMappingURL=new-animation.js.map
